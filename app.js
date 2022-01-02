9b787800c7615440ad52f89d3a2cf52e9715dc7e
danielcr0995 <daniel.correa.1995@gmail.com>
2022-01-02 18:50:23 -0500

    register a new user and check if it already exists

--- app.js
+++ app.js
@@ -3,9 +3,19 @@
 const express=require('express');
 const bodyParser=require('body-parser');
 const ejs = require('ejs');
+const mongoose= require('mongoose');
+
 
 const app= express();
 
+mongoose.connect('mongodb://localhost:27017/secretsDB')
+
+const userSchema=  {
+    username:String,
+    password:String
+};
+
+const User = mongoose.model('User', userSchema);
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended:true})); // read data that is entered in a form
@@ -24,6 +34,27 @@ app.get('/register' ,function(req,res){
     res.render('register');
 })
 
+app.post('/register', function(req,res){
+    const newUsername=req.body.username;
+    const newPassword= req.body.password;
+    User.findOne({username:newUsername}, function(err, foundUser){
+        if(!err){
+            if (foundUser) {
+                console.log('User already exists');
+                
+            }else{
+                const newUser = new User({
+                    username:newUsername,
+                    password:newPassword
+                });
+                newUser.save();
+                console.log('Congratulations, you are now registerd');
+
+            }
+        }
+    })
+    
+})
 
 
 
