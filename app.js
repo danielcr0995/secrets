b1bf488380ae8f6ee8da8296901c20e8244d3439
danielcr0995 <daniel.correa.1995@gmail.com>
2022-01-03 17:09:39 -0500

    login using google auth and style buttons for google and facebook

--- app.js
+++ app.js
@@ -36,7 +36,9 @@ mongoose.connect('mongodb://localhost:27017/secretsDB')
 
 const userSchema= new mongoose.Schema ({ //dont need to add this if you are not doing anything fancy
     username:String,
-    password:String
+    password:String,
+    googleID:String
+
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -48,16 +50,28 @@ const User = mongoose.model('User', userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function (err, user) {
+      done(err, user);
+    });
+  });
 
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets"
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    // userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) { //accesToke allow to get data, refreshToken
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+    // console.log(profile);
+    
+    User.findOrCreate({ googleID: profile.id }, function (err, user) {
       return cb(err, user);
     });
   }
@@ -65,26 +79,38 @@ passport.use(new GoogleStrategy({
 
 app.get('/' ,function(req,res){
     res.render('home');
-})
+});
+
+app.get('/auth/google', 
+    // console.log('redirecting');
+    passport.authenticate('google',  { scope: ['profile'] })
+);
+
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+  });
 
 app.get('/login' ,function(req,res){
     res.render('login');
-})
+});
 
 app.get('/register' ,function(req,res){
     res.render('register');
-})
+});
 
 app.get('/secrets', function(req,res){
     if (req.isAuthenticated()){
         res.render('secrets');
     }else res.redirect('/login');
-})
+});
 
 app.get('/logout', function (req,res) {
     req.logout();
     res.redirect('/');
-})
+});
 
 app.post('/register', function(req,res){
     const newUsername=req.body.username;
@@ -99,7 +125,7 @@ app.post('/register', function(req,res){
                 res.redirect('/secrets'); // will run this only if the user is authenticated
             })
         }
-    })
+    });
     
     
     
@@ -120,12 +146,12 @@ app.post('/login', function (req,res) {
                 res.redirect('/secrets')
             })
         }else console.log(err);
-    })
-})
+    });
+});
 
 
 
 
 app.listen(process.env.PORT || 3000, function(){
     console.log('Server stated on port 3000');
-})
+});
