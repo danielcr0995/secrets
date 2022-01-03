21cf38ae7621c09499ec821fae9b9bcb2b69745f
danielcr0995 <daniel.correa.1995@gmail.com>
2022-01-02 23:11:09 -0500

    add .env files to hide secrets

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //jshint esversion:6
-
+require('dotenv').config()
 const express=require('express');
 const bodyParser=require('body-parser');
 const ejs = require('ejs');
@@ -17,8 +17,8 @@ const userSchema= new mongoose.Schema ({ //dont need to add this if you are not 
     password:String
 });
 
-const secret= '';
-userSchema.plugin(encrypt,{secret:secret, encryptedFields:['password']});
+
+userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:['password']});
 
 const User = mongoose.model('User', userSchema);
 
